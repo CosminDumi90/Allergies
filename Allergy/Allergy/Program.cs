cb9f6cf698477efffe12bfb6ea857c32331500d3
cosmin.dumitru <cosmindumi20@yahoo.com>
2022-01-10 11:17:08 +0200

    first commit

---
+++ Allergy/Allergy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Person;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("cosmin", 31);
+            person.GetAllergensScore(34);
+        }
+    }
+}
---
+++ Allergy/MyAllergies/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Person
+{
+
+    public enum Allergens
+    {
+        eggs = 1,
+
+        peanuts = 2,
+
+        shellfish = 4,
+
+        strawberries = 8,
+
+        tomatoes = 16,
+
+        chocolate = 32,
+
+        pollen = 64,
+
+        cats = 128,
+    };
+    public class Person
+    {
+        private string name;
+        private int age;
+
+
+
+
+        public string Name { get { return this.name; } }
+        public int Age { get { return this.age; } }
+
+
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+
+        }
+
+        public void GetAllergensScore(int score)
+        {
+            int indice = score;
+            if (score >= 256)
+            {
+                Console.WriteLine(" {0} is alergic to: {1}", this.name, Enum.GetName(typeof(Allergens), 1));
+            }
+
+            else
+            {
+                Console.Write(this.name + " is alergic to: ");
+                while (indice >= 1)
+                {
+                    int min = 0;
+                    int wanted = 0;
+                    foreach (int num in Enum.GetValues(typeof(Allergens)))
+                    {
+                        if (num > min && num <= indice)
+                        {
+                            wanted = num;
+                            min = num;
+                        }
+                    }
+                    Console.Write(Enum.GetName(typeof(Allergens), wanted) + " ");
+                    indice -= wanted;
+
+                }
+            }
+        }
+    }
+}
